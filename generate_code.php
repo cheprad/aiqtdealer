437e4b08c870ae2d298e65a415c486aa31466c46
cheprad <68316487+cheprad@users.noreply.github.com>
2022-07-12 17:07:56 +0700

    Update generate_code.php

--- generate_code.php
+++ generate_code.php
@@ -1,4 +1,17 @@
 <?php 
+    $shopnickname = "BAB";
     
+    function generateRandomString($length = 10) {
+        $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+    $code = $shopnickname .generateRandomString(3);
 
+    echo $code .'<br>';
+    echo date("Ymd");
 ?>
