75f7636599caab64e4086e22a6c89b8cdbb64446
cheprad <68316487+cheprad@users.noreply.github.com>
2022-07-14 12:20:57 +0700

    Create realtest_db.sql

---
+++ realtest_db.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.3
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 13, 2022 at 09:32 PM
+-- Server version: 5.5.40
+-- PHP Version: 5.4.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `aiqtdealer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shop`
+--
+
+CREATE TABLE IF NOT EXISTS `shop` (
+  `shopid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `shopname` varchar(255) NOT NULL,
+  `shopdetail` longtext NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `shop`
+--
+
+INSERT INTO `shop` (`shopid`, `userid`, `shopname`, `shopdetail`) VALUES
+(1, 1, 'test', 'test'),
+(2, 3, '123', 'asdsadasdasd'),
+(3, 1, 'asdasdasd', 'testtestt');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `userid` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telnum` varchar(255) NOT NULL COMMENT 'เบอร์โทรศัพท์',
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userid`, `firstname`, `lastname`, `email`, `telnum`, `password`) VALUES
+(15, 'ภรศิษฐ์', 'เกิดบ้านชัน', 'pornrasit.k64@rsu.ac.th', '0610708708', '81dc9bdb52d04dc20036dbd8313ed055'),
+(16, 'Pornrasit1', 'kerdbanchan', 'k_rasit@kkumail.com1', '+66610708708', '81dc9bdb52d04dc20036dbd8313ed055'),
+(17, 'ภรศิษฐ์sad', 'เกิดบ้านชัน', 'pornrasit.k64@rsu.ac.thd', '0610708708', '81dc9bdb52d04dc20036dbd8313ed055'),
+(18, 'pradprad', 'pradprad', 'pradpradprad3@gmail.com', '0610708708', '81dc9bdb52d04dc20036dbd8313ed055');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shop`
+--
+ALTER TABLE `shop`
+  ADD PRIMARY KEY (`shopid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shop`
+--
+ALTER TABLE `shop`
+  MODIFY `shopid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
